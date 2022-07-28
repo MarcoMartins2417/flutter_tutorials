73b5e118ac94753edf4b1425981b95e70a53b224
Marco Martins <marco.martins@exaud.com>
2022-07-28 10:43:02 +0100

    ADAPTIVE_APP - Detection of Different Platforms

--- adaptive_app/lib/main.dart
+++ adaptive_app/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io' show Platform;
+import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
 
 void main() {
@@ -5,63 +7,114 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Adaptive App',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const ResizeablePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+class ResizeablePage extends StatelessWidget {
+  const ResizeablePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final mediaQuery = MediaQuery.of(context);
+    final themePlatform = Theme.of(context).platform;
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+              'Window properties',
+              style: Theme.of(context).textTheme.headline5,
+            ),
+            const SizedBox(height: 10),
+            SizedBox(
+              width: 350,
+              child: Table(
+                textBaseline: TextBaseline.alphabetic,
+                children: <TableRow>[
+                  _fillTableRow(
+                    context: context,
+                    property: 'Window Size',
+                    value: '${mediaQuery.size.width.toStringAsFixed(1)} x '
+                        '${mediaQuery.size.height.toStringAsFixed(1)}',
+                  ),
+                  _fillTableRow(
+                    context: context,
+                    property: 'Device Pixel Ratio',
+                    value: mediaQuery.devicePixelRatio.toStringAsFixed(2),
+                  ),
+                  _fillTableRow(
+                    context: context,
+                    property: 'Platform.isXXX',
+                    value: platformDescription(),
+                  ),
+                  _fillTableRow(
+                    context: context,
+                    property: 'Theme.of(ctx).platform',
+                    value: themePlatform.toString(),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), 
     );
+  }
+
+  TableRow _fillTableRow(
+      {required BuildContext context,
+      required String property,
+      required String value}) {
+    return TableRow(
+      children: [
+        TableCell(
+          verticalAlignment: TableCellVerticalAlignment.baseline,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(property),
+          ),
+        ),
+        TableCell(
+          verticalAlignment: TableCellVerticalAlignment.baseline,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(value),
+          ),
+        ),
+      ],
+    );
+  }
+
+  String platformDescription() {
+    if (kIsWeb) {
+      return 'Web';
+    } else if (Platform.isAndroid) {
+      return 'Android';
+    } else if (Platform.isIOS) {
+      return 'iOS';
+    } else if (Platform.isWindows) {
+      return 'Windows';
+    } else if (Platform.isMacOS) {
+      return 'macOS';
+    } else if (Platform.isLinux) {
+      return 'Linux';
+    } else if (Platform.isFuchsia) {
+      return 'Fuchsia';
+    } else {
+      return 'Unknown';
+    }
   }
 }
