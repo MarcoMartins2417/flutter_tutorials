76844248e309563cfe1f1d94dde5803dd00a3045
Marco Martins <marco.martins@exaud.com>
2022-07-28 12:39:16 +0100

    ADAPTIVE_APP - Changes in the main Code to Desktop Adaptation and Add of new Playlist File

--- adaptive_app/lib/main.dart
+++ adaptive_app/lib/main.dart
@@ -4,15 +4,16 @@ import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import 'src/adaptive_playlists.dart';
 import 'src/app_state.dart';
-import 'src/playlists.dart';
+
 
 const flutterDevAccountId = 'UCwXdFgeE9KYzlDdR7TG9cMw';
 
 const youTubeApiKey = 'AIzaSyCKJ2b_xxk8r8Iff8-yNk3Vi3jmk64JPv0';  // API Key 
 
 void main() {
-  if (youTubeApiKey == 'AIzaNotAnApiKey') {
+  if (youTubeApiKey == 'AIzaSyCKJ2b_xxk8r8Iff8-yNk3Vi3jmk64JPv0') {
     print('youTubeApiKey has not been configured.');
     exit(1);
   }
@@ -35,9 +36,9 @@ class PlaylistsApp extends StatelessWidget {
       title: 'FlutterDev Playlists',
       theme: FlexColorScheme.light(scheme: FlexScheme.red).toTheme,
       darkTheme: FlexColorScheme.dark(scheme: FlexScheme.red).toTheme,
-      themeMode: ThemeMode.dark, // Or ThemeMode.System if you'd prefer
+      themeMode: ThemeMode.dark, 
       debugShowCheckedModeBanner: false,
-      home: const Playlists(),
+      home: const AdaptivePlaylists(),
     );
   }
 }
