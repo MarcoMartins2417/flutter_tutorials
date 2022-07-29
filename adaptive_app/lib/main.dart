8da0a3ea18f02f8dc4fe0da581cf3e70562ab110
MarcoMartins2417 <110040554+MarcoMartins2417@users.noreply.github.com>
2022-07-29 16:52:47 +0100

    Adaptive app (#2)
    * ADAPTIVE_APP - Created Adaptive App Project
    
    * ADAPTIVE_APP - Detection of Different Platforms
    
    * ADAPTIVE_APP - Add Dependencies
    
    * ADAPTIVE_APP - Configuration of url_launcher Plugin for Mobile Apps
    
    * ADAPTIVE_APP - Creation of the Playlist Files
    
    * ADAPTIVE_APP - Add of new Dependencies needed for Web Adaptation and Removal of Unnecessary ones
    
    * ADAPTIVE_APP - Add New Dependency and  Change Server File
    
    * ADAPTIVE_APP - Add an Import to the Playlists File and Change the Image Type

---
+++ adaptive_app/lib/main.dart
@@ -0,0 +1,38 @@
+import 'dart:io';
+
+import 'package:flex_color_scheme/flex_color_scheme.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'src/adaptive_playlists.dart';
+import 'src/app_state.dart';
+
+const flutterDevAccountId = 'UCwXdFgeE9KYzlDdR7TG9cMw';
+
+const youTubeApiKey = 'AIzaSyCKJ2b_xxk8r8Iff8-yNk3Vi3jmk64JPv0';  // API Key 
+
+void main() {
+  runApp(ChangeNotifierProvider<FlutterDevPlaylists>(
+    create: (context) => FlutterDevPlaylists(
+      flutterDevAccountId: flutterDevAccountId,
+      youTubeApiKey: youTubeApiKey,
+    ),
+    child: const PlaylistsApp(),
+  ));
+}
+
+class PlaylistsApp extends StatelessWidget {
+  const PlaylistsApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'FlutterDev Playlists',
+      theme: FlexColorScheme.light(scheme: FlexScheme.red).toTheme,
+      darkTheme: FlexColorScheme.dark(scheme: FlexScheme.red).toTheme,
+      themeMode: ThemeMode.dark, 
+      debugShowCheckedModeBanner: false,
+      home: const AdaptivePlaylists(),
+    );
+  }
+}
