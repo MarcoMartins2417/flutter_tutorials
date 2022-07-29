01d1563667f50d4f9d7e7aa040b30d6fd77fb663
Marco Martins <marco.martins@exaud.com>
2022-07-29 15:18:37 +0100

    ADAPTIVE_APP - Removal of extra Lines and Removal of Unnecessary Code

--- adaptive_app/lib/main.dart
+++ adaptive_app/lib/main.dart
@@ -7,17 +7,11 @@ import 'package:provider/provider.dart';
 import 'src/adaptive_playlists.dart';
 import 'src/app_state.dart';
 
-
 const flutterDevAccountId = 'UCwXdFgeE9KYzlDdR7TG9cMw';
 
 const youTubeApiKey = 'AIzaSyCKJ2b_xxk8r8Iff8-yNk3Vi3jmk64JPv0';  // API Key 
 
 void main() {
-  if (youTubeApiKey == 'AIzaSyCKJ2b_xxk8r8Iff8-yNk3Vi3jmk64JPv0') {
-    print('youTubeApiKey has not been configured.');
-    exit(1);
-  }
-
   runApp(ChangeNotifierProvider<FlutterDevPlaylists>(
     create: (context) => FlutterDevPlaylists(
       flutterDevAccountId: flutterDevAccountId,
